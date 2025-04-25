5c2ec9edc94b5c399d29c49b488ca1bbd3e6ea84
Elias-Karlsson <eliasjonkarlsson@gmail.com>
2025-04-25 03:30:31 +0200

    Updated autoplay and play/pause functionality for 2amtext.html

--- js/script.js
+++ js/script.js
@@ -340,11 +340,12 @@ To generate new ads just click the case on the left again.<br> <br>
                 </div>
             `,
             banned: `
-                <br> a radio that only broadcasts from wherever it’s 2AM. <br> https://elias-portfolio.github.io/work/radiomp3.html
+              
                     <div id="iframe-container" style="margin-top: 20px; width: 90%; height: 700px; overflow: hidden;">
                         <!-- Iframe directly displaying the ground6.html content -->
-                        <iframe id="ground5-iframe" src="radiomp33.html" style="width: 100%; height: 100%; border: none;"></iframe>
+                        <iframe id="ground5-iframe" src="2amtext.html" style="width: 100%; height: 100%; border: none;"></iframe>
                     </div> 
+  <br> a radio that only broadcasts from wherever it’s 2AM. <br> https://elias-portfolio.github.io/work/2amtext.html
             `,
             kindle: `
                 <div id="quotation" onclick="fetchAndDisplayQuotation();">
